8344b951800d29528263abc666f9e96bbfd51141
Jonathan Hampton <hamptonjc221@gmail.com>
2023-01-15 19:37:24 -0800

    lay out training modules

--- training_modules.py
+++ training_modules.py
@@ -1,10 +1,9 @@
 # Imports
 from abc import abstractmethod
-from typing import Any
 import torch
 import pytorch_lightning as pl
 from utils import MasterConfig
-from networks import Vnet
+from networks import Vnet, Mnet, Cnet
 
 
 # PTL modules
@@ -21,16 +20,44 @@ class TrainingModule(pl.LightningModule):
         self.config = config
 
     @abstractmethod
-    def training_step(self) -> Any:
+    def training_step(self) -> torch.Tensor:
         raise NotImplementedError()
 
     @abstractmethod
-    def validation_step(self) -> Any:
+    def validation_step(self) -> torch.Tensor:
         raise NotImplementedError()
 
 
-class VnetLitModule(TrainingModule):
+class VnetTrainingModule(TrainingModule):
 
     def __init__(self, config: MasterConfig) -> None:
         super().__init__(config)
         self.net = Vnet(config)
+
+    def training_step(self, image: torch.Tensor) -> torch.Tensor:
+        recon_image, _ = self.net(image)
+        loss = self.loss_function(image, recon_image)
+        return loss
+
+    def validation_step(self, image: torch.Tensor) -> torch.Tensor:
+        reconstructed, _ = self.net(image)
+        loss = self.loss_function(image, reconstructed)
+        return loss
+
+    def loss_function(self, original: torch.Tensor,
+                      reconstructed: torch.Tensor) -> torch.Tensor:
+        ...
+
+
+class MnetTrainingModule(TrainingModule):
+
+    def __init__(self, config: MasterConfig) -> None:
+        super().__init__(config)
+        self.net = Mnet(config)
+
+
+class CnetTrainingModule(TrainingModule):
+
+    def __init__(self, config: MasterConfig) -> None:
+        super().__init__(config)
+        self.net = Cnet(config)
