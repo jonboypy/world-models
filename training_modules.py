b22d83d315e968ea57783a1507d261deaaf10402
hamptonjc <hamptonjc221@gmail.com>
2023-03-20 22:55:47 -0700

    finish initial M-Net training module

--- training_modules.py
+++ training_modules.py
@@ -30,12 +30,20 @@ class TrainingModule(pl.LightningModule):
 
 class VnetTrainingModule(TrainingModule):
     """
-    Training module for training the V-network (VAE).
+    Training module for training the 'V' vision network.
     """
 
     def __init__(self, config: MasterConfig) -> None:
         super().__init__(config)
         self.net = Vnet(config)
+
+    def loss_function(self, original: torch.Tensor,
+                      reconstructed: torch.Tensor,
+                      latent: torch.Tensor) -> torch.Tensor:
+        generative_loss = torch.nn.MSELoss()(reconstructed, original)
+        latent_loss = torch.nn.KLDivLoss()(latent, torch.randn_like(latent))
+        loss = generative_loss + latent_loss
+        return loss
 
     def training_step(self, image: torch.Tensor) -> torch.Tensor:
         reconstructed, latent = self.net(image)
@@ -47,31 +55,49 @@ class VnetTrainingModule(TrainingModule):
         loss = self.loss_function(image, reconstructed, latent)
         return loss
 
-    def loss_function(self, original: torch.Tensor,
-                      reconstructed: torch.Tensor,
-                      latent: torch.Tensor) -> torch.Tensor:
-        generative_loss = torch.nn.MSELoss()(reconstructed, original)
-        latent_loss = torch.nn.KLDivLoss()(latent, torch.randn_like(latent))
-        loss = generative_loss + latent_loss
-        return loss
-
 
 class MnetTrainingModule(TrainingModule):
-
+    """
+    Training module for training the 'M' memory network.
+    """
+    
     def __init__(self, config: MasterConfig) -> None:
         super().__init__(config)
         self.net = Mnet(config)
 
-
     def loss_function(self,
-            mixture: torch.distribution.Distribution,
-            true_z1: torch.Tensor) -> torch.Tensor:
+            z_next_est_dist: torch.distribution.Distribution,
+            z_next: torch.Tensor) -> torch.Tensor:
         # negative log probability
-        return -mixture.log_prob(true_z1)
+        #TODO: might need more sophisticated
+        #       form of liklihood calculation
+        return -z_next_est_dist.log_prob(z_next)
+
+    def training_step(self, batch: torch.Tensor) -> torch.Tensor:
+        # unpack batch
+        # (B,S,Z), (B,S,A)
+        z_prev, a_prev, z_next = batch
+        z_next_est_dist = self.net(z_prev, a_prev)
+        loss = self.loss_function(z_next_est_dist, z_next)
+        return loss
+
+    def validation_step(self, batch: torch.Tensor) -> torch.Tensor:
+        # unpack batch
+        # (B,S,Z), (B,S,A)
+        z_prev, a_prev, z_next = batch
+        z_next_est_dist = self.net(z_prev, a_prev)
+        loss = self.loss_function(z_next_est_dist, z_next)
+        return loss
 
 
 class CnetTrainingModule(TrainingModule):
+    """
+    Module designed for 'Covariance-Matrix
+    Adaptation Evolution Strategy' to train the
+    'C' controller network.
+    """
 
     def __init__(self, config: MasterConfig) -> None:
         super().__init__(config)
         self.net = Cnet(config)
+
