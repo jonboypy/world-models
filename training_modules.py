3af8275ca49a19a879994796626dc8b9eace60d1
hamptonjc <hamptonjc221@gmail.com>
2023-03-26 22:25:29 -0700

    Implement C-Net optimization algorithm

--- training_modules.py
+++ training_modules.py
@@ -1,9 +1,15 @@
 # Imports
+from typing import Tuple
+import copy
 from abc import abstractmethod
 import torch
 import pytorch_lightning as pl
+import ray
+from tqdm import tqdm
 from utils import MasterConfig
 from networks import Vnet, Mnet, Cnet
+from environments import GymEnvironment
+from agents import WorldModelGymAgent
 
 
 # PTL modules
@@ -90,7 +96,7 @@ class MnetTrainingModule(TrainingModule):
         return loss
 
 
-class CnetTrainingModule(TrainingModule):
+class CnetTrainingModule:
     """
     Module designed for 'Covariance-Matrix
     Adaptation Evolution Strategy' to train the
@@ -98,6 +104,106 @@ class CnetTrainingModule(TrainingModule):
     """
 
     def __init__(self, config: MasterConfig) -> None:
-        super().__init__(config)
-        self.net = Cnet(config)
+        self.config = config
+        self.pop_size = config.POP_SIZE
+        self.top_percentage = config.TOP_PERCENTAGE
+        self.epochs = config.EPOCHS
+        self._load_modules()
+        self._initialize()
 
+    def _load_modules(self) -> None:
+        self.cnet = Cnet(self.config)
+        if hasattr(self, 'TEST'):
+            self.vnet_encoder = Vnet(self.config).encoder
+            self.mnet = Mnet(self.config)
+        else:
+            self.vnet_encoder = VnetTrainingModule.\
+                load_from_checkpoint(
+                self.config.VNET_CKPT,
+                config = self.config).net.encoder
+            self.mnet = MnetTrainingModule.\
+                load_from_checkpoint(
+                self.config.MNET_CKPT,
+                config = self.config).net
+
+    def _initialize(self) -> Tuple[torch.Tensor]:
+        # Calculate the number of parameters in C-Net
+        self.event_space = self.cnet.l1.weight.numel() + \
+                            self.cnet.l1.bias.numel()
+        # Initialize random location & covariance matricies
+        mu = torch.rand(self.event_space)
+        sigma = torch.eye(self.event_space)
+        return mu, sigma
+
+    def optimize(self) -> None:
+        # Get initial mu & sigma
+        mu, sigma = self._initialize()
+        # For every epoch...
+        for e in tqdm(range(self.epochs), description='Optimizing'):
+            # Create a sample space based on the current mu & sigma
+            space = torch.distributions.MultivariateNormal(mu, sigma)
+            # Sample a population of parameters from the space
+            population = space.sample((self.pop_size,))
+            # Evaluate each set of parameters in population
+            scores = self._evaluate_population(population)
+            # Get the top % of the population
+            best = torch.topk(scores, int(
+                self.pop_size * self.top_percentage))
+            # Calculate new covariance matrix
+            var = ((population[best.indices] - mu).pow(2)).mean(0)
+            cov = ((population[best.indices] - mu).prod(1)).mean(0)
+            sigma = torch.diag(cov.view(1,),1) + \
+                        torch.diag(cov.view(1,),-1) + \
+                            torch.diag(var)
+            # Enforce sigma to be greater than 0
+            sigma = sigma.clamp(1e-32)
+            # Calculate new mu
+            mu = population[best.indices].mean(0)
+            # Perform evaluation of top performer to log algorithm progress
+            if e % self.config.TEST_EVERY_N_EPOCHS == 0:
+                score = self._evaluate_individual(
+                    population[best.indices][0],
+                    self.config.TEST_N_ROLLOUTS)
+                self.log(score)
+        # TODO: Save best individual
+
+    def _evaluate_population(self,
+            population: torch.Tensor) -> torch.Tensor:
+        # Launch parallel evaluations of each individual in population
+        cfg = copy.deepcopy(self.config)
+        scores = ray.get([self._evaluate_individual.remote(
+                          cfg,
+                          population[i],
+                          self.vnet_encoder, self.mnet,
+                          self.config.EVAL_N_ROLLOUTS)
+                          for i in range(self.pop_size)]) 
+        return scores
+    
+    @staticmethod
+    @ray.remote
+    def _evaluate_individual(config: MasterConfig,
+                             individual: torch.Tensor,
+                             vnet_encoder: torch.nn.Module,
+                             mnet: torch.nn.Module,
+                             n_rollouts: int) -> torch.Tensor:
+        # Load parameters into C-Net
+        cnet = Cnet(config)
+        # Reshape params to size of weight matrix (+1 for bias vector)
+        individual = individual.reshape(cnet.l1.weight.size(0),
+                                        cnet.l1.weight.size(1)+1)
+        # Load params into net
+        cnet.l1.weight = torch.nn.Parameter(individual[:,:-1], False)
+        cnet.l1.bias = torch.nn.Parameter(individual[:,-1], False)
+        # Make environment
+        env = GymEnvironment(config)
+        env.reset()
+        # Make agent
+        agent = WorldModelGymAgent(env, vnet_encoder,
+                                   mnet, cnet)
+        # Run rollouts
+        n_eps = 0
+        while n_eps < n_rollouts: #TODO could parallelize the rollouts too?
+            n_eps += agent.act()
+        # Get fitness score of individual (i.e. mean return)
+        score = agent.avg_cum_reward
+        return score
