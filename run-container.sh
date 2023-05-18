46eccfc6a8791521db280d14b70261e411350ebe
Jonathan Hampton <hamptonjc221@gmail.com>
2023-05-17 23:34:54 -0700

    Increase shm and set to priveleged (NVIDIA BUG)

--- run-container.sh
+++ run-container.sh
@@ -4,6 +4,7 @@ docker run \
     --mount type=bind,source="$(pwd)",target=/world-models \
     --gpus all \
     -it \
+    --privileged \
     --name world-models \
     -w /world-models \
     --rm \
@@ -12,5 +13,6 @@ docker run \
     -v="/etc/passwd:/etc/passwd:ro" \
     -v="/etc/shadow:/etc/shadow:ro" \
     -v="/etc/sudoers:/etc/sudoers:ro" \
+    --shm-size=4G \
     world-models:latest \
     bash
